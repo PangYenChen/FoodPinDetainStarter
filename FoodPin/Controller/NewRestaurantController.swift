0bb1b2391db69e76f32bcdb09d9b476a02da0b89
PangYen Chen <cpy4869@gmail.com>
2019-05-29 11:29:10 +0800

    18章作業一完成

--- FoodPin/Controller/NewRestaurantController.swift
+++ FoodPin/Controller/NewRestaurantController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class NewRestaurantController: UITableViewController {
     
+    @IBOutlet var photoImageView: UIImageView!
+    
     @IBOutlet var nameTextField: RoundTextField! {
         didSet {
             nameTextField.tag = 1
@@ -50,7 +52,58 @@ class NewRestaurantController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //設定導覽列
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.shadowImage = UIImage()
+        if let customFont = UIFont(name: "Rubik-Medium", size: 35) {
+            navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(red: 231, green: 76, blue: 60), NSAttributedString.Key.font: customFont]
+        }
     }
+    
+    //MARK: - Table View Delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard indexPath.row == 0 else {
+            return
+        }
+        
+        let photoSourceRequestController = UIAlertController(title: "", message: "choose your photo source", preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { (action) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                let imagePicker = UIImagePickerController()
+                imagePicker.allowsEditing = false
+                imagePicker.sourceType = .camera
+                
+                imagePicker.delegate = self
+                
+                self.present(imagePicker, animated: true, completion: nil)
+            }
+        }
+        photoSourceRequestController.addAction(cameraAction)
+        
+        let photoLibraryAction = UIAlertAction(title: "Photo library", style: .default) { (action) in
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                let imagePicker = UIImagePickerController()
+                imagePicker.allowsEditing = false
+                imagePicker.sourceType = .photoLibrary
+                
+                imagePicker.delegate = self
+                
+                self.present(imagePicker, animated: true, completion: nil)
+            }
+        }
+        photoSourceRequestController.addAction(photoLibraryAction)
+        
+        if let popoverController = photoSourceRequestController.popoverPresentationController,
+            let cell = tableView.cellForRow(at: indexPath) {
+            popoverController.sourceView = cell
+            popoverController.sourceRect = cell.bounds
+        }
+        
+        present(photoSourceRequestController, animated: true, completion: nil)
+        
+    }
+    
 }
 
 
@@ -64,3 +117,37 @@ extension NewRestaurantController: UITextFieldDelegate {
         return true
     }
 }
+
+
+extension NewRestaurantController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            photoImageView.image = selectedImage
+            photoImageView.contentMode = .scaleToFill
+            photoImageView.clipsToBounds = true
+        }
+        
+        let leadingConstraint = NSLayoutConstraint(item: photoImageView
+            , attribute: .leading, relatedBy: .equal, toItem: photoImageView.superview, attribute: .leading, multiplier: 1, constant: 0)
+        leadingConstraint.isActive = true
+        
+        let trailingConstraint = NSLayoutConstraint(item: photoImageView
+            , attribute: .trailing, relatedBy: .equal, toItem: photoImageView.superview, attribute: .trailing, multiplier: 1, constant: 0)
+        trailingConstraint.isActive = true
+        
+        let topConstraint = NSLayoutConstraint(item: photoImageView
+            , attribute: .top, relatedBy: .equal, toItem: photoImageView.superview, attribute: .top, multiplier: 1, constant: 0)
+        topConstraint.isActive = true
+        
+        let bottomConstraint = NSLayoutConstraint(item: photoImageView
+            , attribute: .bottom, relatedBy: .equal, toItem: photoImageView.superview, attribute: .bottom, multiplier: 1, constant: 0)
+        leadingConstraint.isActive = true
+        
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+
+
+extension NewRestaurantController: UINavigationControllerDelegate {}
